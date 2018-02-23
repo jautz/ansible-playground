52ebe25dea7c719d49afd8da93866aba6bf3e064
Joachim Jautz <github@jautz.org.nospam>
2018-02-23 17:58:54 +0100

    Remove anything not needed for the interpreter demo use case.

--- roles/bozo/library/vicontrol.py
+++ roles/bozo/library/vicontrol.py
@@ -116,7 +116,7 @@ def run_module():
 
     # in the event of a successful module execution, you will want to
     # simple AnsibleModule.exit_json(), passing the key/value results
-    sleep(10)
+    sleep(2)
     module.exit_json(**result)
 
 def main():
